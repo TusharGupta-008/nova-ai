12553008a46507ee67b520977c5d20a36f4bc849
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-07-12 21:28:11 +0530

    changed the client url

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import groqResponse from "./llm.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
